0eaab93edf89b5e2fb137a43b7a531ccecb0bf1b
dianalopz <dlauralr@icloud.com>
2018-06-14 11:28:57 -0500

    Commit2

--- src/cipher.js
+++ src/cipher.js
@@ -8,23 +8,32 @@ function encriptar() {
   event.preventDefault()
   //Inicialisa arreglo
   let resultado = [];
+  let nuevaPosicion = 0;
   //obtener el msj y guardarlo en una variable
   const mensaje = document.getElementById("msj").value;
+  //Obtener el num de saltos
+  const numSaltos= document.getElementById("saltos").value;
   //Convertir a mayuscula
   const msjEnMayuscula = mensaje.toUpperCase();
   //Obtiene la longitud del msj
   for (let contador = 0; contador < mensaje.length; contador++) {
     //Obtener la posicion de la primerletra en Ascii
     let posicionAscii = msjEnMayuscula.charCodeAt(contador);
-    //Aplicar formula
-    let nuevaPosicion = (posicionAscii-65+33)%26+65;
+    //Detecta espacios en blanco
+    if (posicionAscii == 32) {
+      nuevaPosicion = 32;
+    } else {
+      //Aplicar formula
+      nuevaPosicion = (posicionAscii-65+parseInt(numSaltos))%26+65;
+    }
     //Extraer la nueva posicion en el arreglo
     resultado.push(String.fromCharCode(nuevaPosicion));
+    var final = resultado.join('');
     //document.write (resultado);
-    console.log(resultado);
   }
-  document.getElementById("imprimir").innerHTML = "Tu mensaje encriptado es: " + resultado + " guarda el secreto ;)";
+  document.getElementById("imprimir").innerHTML = "Tu mensaje encriptado es: " + final;
 };
+
 
 //Funcion para Desencriptar
 function desencriptar() {
@@ -32,20 +41,29 @@ function desencriptar() {
   event.preventDefault()
   //Inicialisa arreglo
   let resultado = [];
+  let nuevaPosicion = 0;
   //obtener el msj y guardarlo en una variable
   const mensajeEncriptado = document.getElementById("msj").value;
+  //Obtener el num de saltos
+  const numSaltos= document.getElementById("saltos").value;
   //Convertir a mayuscula
   const msjEncriptadoEnMayuscula = mensajeEncriptado.toUpperCase();
   //Obtiene la longitud del msj
   for (let contador = 0; contador < mensajeEncriptado.length; contador++) {
     //Obtener la posicion de la primerletra en Ascii
     let posicionAscii = msjEncriptadoEnMayuscula.charCodeAt(contador);
-    //Aplicar formula
-    let nuevaPosicion = posicionAscii-7;
+    //Detecta espacios en blanco
+    if (posicionAscii == 32) {
+      nuevaPosicion = 32;
+    } else {
+      //Aplicar formula
+      nuevaPosicion = (posicionAscii+65-parseInt(numSaltos))%26+65;
+    }
     //Extraer la nueva posicion en el arreglo
     resultado.push(String.fromCharCode(nuevaPosicion));
+    var final = resultado.join('');
     //return resultado;
     console.log(resultado);
   }
-  document.getElementById("imprimir").innerHTML = "Tu mensaje real es: " + resultado;
+  document.getElementById("imprimir").innerHTML = "Tu mensaje real es: " + final;
 };
